2a1e36bea6571ba6d58d6cdbeaaa62e07553b7d0
侯磊 <foxhl0907@gmail.com>
2018-11-12 23:45:38 +0800

    工程结构搭建

---
+++ webApp/hllhc/src/config/axios.js
@@ -0,0 +1,77 @@
+import Axios from 'axios'
+
+export function urlSearch () {
+  const result = {}
+  let name, value
+  let str = location.href // 取得整个地址栏
+  let num = str.indexOf('?')
+  str = str.substr(num + 1) // 取得所有参数   stringvar.substr(start [, length ]
+
+  const arr = str.split('&') // 各个参数放到数组里
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i].indexOf('=')
+    if (num > 0) {
+      name = arr[i].substring(0, num)
+      value = arr[i].substr(num + 1)
+      result[name] = value
+    }
+  }
+  return result
+}
+
+function initAccessToken (accesstoken) {
+  if (accesstoken) {
+    sessionStorage.setItem('atk', accesstoken)
+  }
+}
+
+// import config from './devconfig.js'
+/**
+ * vue 安装 axios 配置方法
+ * 调用Vue.use 可执行该方法
+ * 目的是将axios 挂载到全局的实例中
+ */
+function install (Vue, router) {
+  initAccessToken(urlSearch().atk)
+
+  const axios = Axios.create({
+    //  baseURL: config.baseURL
+    baseURL: ''
+  })
+  axios.defaults.withCredentials = true
+  Vue.prototype.$axios = axios
+  interceptors(axios, Vue, router)
+}
+
+/**
+ * 全局返回数据拦截
+ */
+function interceptors (axios, Vue, router) {
+  axios.interceptors.request.use(function (config) {
+    // guest请求无需进行身份验证
+    if (config.guest === true) {
+    } else {
+      if (sessionStorage.getItem('atk')) {
+        config.headers['Authorization'] = 'bearer ' + sessionStorage.getItem('atk')
+      }
+    }
+    // 修改请求设置
+    return config
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  })
+
+  // 截获返回数据，对请求状态进行统一处理
+  axios.interceptors.response.use(function (response) {
+    return response
+    // 相应设置
+  }, function (error) {
+    return Promise.reject(error.response)
+  })
+}
+
+export {
+  install,
+  interceptors
+}
---
+++ webApp/hllhc/src/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+    <div>
+      id:<el-input v-model="id"></el-input>
+      <el-button @click="submit">提交</el-button>
+    </div>
+    <div>
+      <el-input v-model="data"></el-input>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      id: '',
+      data: 'default value!'
+    }
+  },
+  methods: {
+    submit () {
+      this.$axios.post('api/Handler.ashx?id=' + this.id).then(res => {
+        this.data = res.data
+      })
+    }
+  }
+}
+</script>
